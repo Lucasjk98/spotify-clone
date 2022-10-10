a7a3b16c6555a241c2ab09ba2935b8ed9c2b51dc
lucasjk98 <lucasjimenezk1998@gmail.com>
2022-10-10 16:17:36 -0600

    thotify ;)

--- src/Login.js
+++ src/Login.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './Login.css';
 import { loginUrl } from "./spotify";
 
+
 function Login (){
     return (
         <div className="login">
-            <img src = "https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt='' />
-            <a href = {loginUrl}> LOGIN with spotify </a>
+            <img src = 'thotify.webp' alt='' />
+            <h2> Essentially Spotify but shittier </h2> 
+            <a href = {loginUrl}> LINK with spotify or OnlyFans </a>
 
         </div>
     )
